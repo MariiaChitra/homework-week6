0c245ed6c1e930ba9278dc8435cdbc52f7b4101b
Mariia <mariia.kushnir.personal@gmail.com>
2022-07-04 10:07:56 +0300

    Added weather project

---
+++ script.js
@@ -0,0 +1,83 @@
+let now = new Date();
+let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+let minute = now.getMinutes();
+if (minute < 10) {
+  minute = `0${minute}`;
+}
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+
+let h5 = document.querySelector("h5");
+h5.innerHTML = `${hour}:${minute} ${day} ${now.getDate()} ${month} ${now.getFullYear()}`;
+
+function searchPlace(city) {
+  let apiKey = "97852d4a29a006cb23ee815273ca6a59";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherDetails);
+}
+
+function displayWeatherDetails(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#feels_like").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function displayWeather(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input").value;
+  searchPlace(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "97852d4a29a006cb23ee815273ca6a59";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherDetails);
+}
+
+function actualPlaceLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let replaceLocation = document.querySelector("#form");
+replaceLocation.addEventListener("submit", displayWeather);
+
+let currentLocationButton = document.querySelector("#current_city");
+currentLocationButton.addEventListener("click", actualPlaceLocation);
+searchPlace("Bat Yam");
